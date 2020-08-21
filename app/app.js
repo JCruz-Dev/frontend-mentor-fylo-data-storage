3df250787e9c2533657fb37ca40f403d5c1bdaa7
Juan carlos <juancarloscruz278@gmail.com>
2020-08-21 01:23:05 -0400

    Creates logic for gigabyte bar storage

--- app/app.js
+++ app/app.js
@@ -1,1 +1,25 @@
-console.log(' Ya tu sabe')
+const mainBar =  document.querySelector('.bar-container').clientWidth;
+let insideBar = document.querySelector('.bar-inside');
+let storageInfo = document.querySelector('.info-left-number');
+let barSeconds = 30, textSeconds = 10, barWith = 0, gigaLeft = 0, spaceLeft = 185;
+
+function animateBarWidth(){
+   if(barWith !== parseInt(insideBar.dataset.size)){
+       barWith++;
+      insideBar.style.width = `${barWith}%`
+   }
+}
+function animateNumberLeft(){
+    if(gigaLeft !== spaceLeft){
+        gigaLeft++;
+       storageInfo.textContent = `${gigaLeft}`
+    }
+}
+
+setInterval(()=> {
+    animateBarWidth()
+}, barSeconds)
+
+setInterval(() => {
+    animateNumberLeft()
+}, textSeconds)
